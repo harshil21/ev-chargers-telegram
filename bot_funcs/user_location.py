523cd5135c06edccd18f81b331cd5dd5ccaa3e63
Harshil <37377066+harshil21@users.noreply.github.com>
2023-01-03 22:10:13 +0400

    improvements to cleanup, and markup

--- bot_funcs/user_location.py
+++ bot_funcs/user_location.py
@@ -1,3 +1,4 @@
+import logging
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, Location
 from telegram.ext import ContextTypes
 from telegram.error import BadRequest
@@ -15,6 +16,8 @@ from api.distance_calc import distance_in_km
 
 from datetime import timedelta
 
+LIVE_PERIODS = [900, 3600, 28800]  # 15 minutes, 1 hour, 8 hours
+
 
 async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """If the user sent a regular location, ask them to send a live location instead"""
@@ -25,31 +28,33 @@ async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # `live_period` attribute.
     location = update.effective_message.location
     user_id = update.effective_user.id
-    print(location)
+    live_p = location.live_period
+    logging.info(location)
 
     # If user has sent a regular location, ask them to send a live location instead
-    if not location.live_period and not location.horizontal_accuracy:
+    if not live_p and not location.horizontal_accuracy:
         # rare case where user actually ended live location sharing, but somehow didn't have 
         # horizontal_accuracy set
         if jobs:=context.job_queue.get_jobs_by_name(str(user_id)):
             jobs[0].schedule_removal()
-            await cleanup(context)
+            context.job_queue.run_once(cleanup, 3, user_id=user_id, name=str(user_id))
             return 
 
         await update.effective_message.reply_text(no_static_location_msg)
         return
 
     # If user has ended sharing live location, we need to clean up
-    if location.horizontal_accuracy and not location.live_period:
+    if location.horizontal_accuracy and (not live_p or live_p not in LIVE_PERIODS):
         # User has ended sharing live location manually
         # Remove the job so we don't run it twice
+        print("ended sharing live location")
         context.job_queue.get_jobs_by_name(str(user_id))[0].schedule_removal()
-        await cleanup(context)
+        context.job_queue.run_once(cleanup, 3, user_id=user_id, name=str(user_id))
         return
 
     # we add 5 seconds to the live_period, because the last update we get from Telegram
     # could come a few seconds late
-    when_to_run = timedelta(seconds=location.live_period + 5) + update.effective_message.date
+    when_to_run = timedelta(seconds=live_p + 5) + update.effective_message.date
 
     # Schedule a job to run when the live location sharing ends
     if not context.job_queue.get_jobs_by_name(str(user_id)):  # Don't schedule more than one job
@@ -61,7 +66,10 @@ async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # If user has started new live location, we need to share the list of chargers near them
     if "list_of_chargers" not in context.user_data:
         # If there are no chargers near the user, we will alert them when we find some
-        if not chargers:
+        markup = await generate_markup(chargers, location, context)
+        buttons_in_markup = await get_number_of_markup_buttons(markup)
+
+        if not chargers or not buttons_in_markup:
             print("No chargers near you")
             # If we have already alerted the user that there are no chargers near them, don't do it again
             if not context.user_data.get("no_chargers_found", False):
@@ -69,12 +77,11 @@ async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
             context.user_data["no_chargers_found"] = True
             return
 
-        markup = await generate_markup(chargers, location, context)
-
         msg = await update.effective_message.reply_text(
             found_chargers_near_you.format(number_of_chargers=len(chargers)), 
             reply_markup=markup
         )
+        print("Found chargers near you")
         context.user_data["editing_ids"] = (msg.chat_id, msg.message_id)
         context.user_data["no_chargers_found"] = False
         return
@@ -82,24 +89,31 @@ async def handle_location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Check if we have a new charger near the user, that is not already in the list
     new_chargers = [i for i in chargers if i not in context.user_data["list_of_chargers"]]
 
+    all_chargers = context.user_data["list_of_chargers"] + new_chargers
+    # and just update the list of chargers by editing reply markup
+    markup = await generate_markup(all_chargers, location, context)
+    length_of_markup = await get_number_of_markup_buttons(markup)
+
     # If there are new chargers, alert the user
-    if new_chargers:
+    if new_chargers and length_of_markup:
         print("New chargers found")
         await update.effective_message.reply_text(new_chargers_near_you)
         context.user_data["no_chargers_found"] = False
 
-    all_chargers = context.user_data["list_of_chargers"] + new_chargers
-
-    # and just update the list of chargers by editing reply markup
-    markup = await generate_markup(all_chargers, location, context)
     chat_id, message_id = context.user_data["editing_ids"]
     try:
-        msg = await context.bot.edit_message_reply_markup(chat_id, message_id, reply_markup=markup)
+        msg = await context.bot.edit_message_text(
+            text=found_chargers_near_you.format(number_of_chargers=len(chargers)),
+            chat_id=chat_id, 
+            message_id=message_id, 
+            reply_markup=markup
+        )
     except BadRequest:  # Message reply markup is the same
         pass
     else:
         context.user_data["no_chargers_found"] = False
         context.user_data["editing_ids"] = (msg.chat_id, msg.message_id)
+        print("edited message with new markup")
 
 
 async def generate_markup(chargers: list[dict], location: Location, context: ContextTypes.DEFAULT_TYPE) -> InlineKeyboardMarkup:
@@ -107,8 +121,8 @@ async def generate_markup(chargers: list[dict], location: Location, context: Con
 
     # cache the distance between the user and each charger
     cached_distances = {(i["lat"], i["lng"]): distance_in_km((location.latitude, location.longitude), (i["lat"], i["lng"])) for i in chargers}
-    # Drop chargers that are more than 10km away
-    chargers = [i for i in chargers if cached_distances[(i["lat"], i["lng"])] <= 10]
+    # Drop chargers that are more than 50km away
+    chargers = [i for i in chargers if cached_distances[(i["lat"], i["lng"])] <= 50]
 
     # Save list of new chargers, after we have dropped the ones that are too far away
     context.user_data["list_of_chargers"] = chargers
@@ -119,10 +133,18 @@ async def generate_markup(chargers: list[dict], location: Location, context: Con
             f"{i['name']} ({round(cached_distances[(i['lat'], i['lng'])], 2)}km)",
             url=MAPS_LINK.format(lat=i['lat'], lng=i['lng'])
         )
-        for i in chargers],
+        for i in chargers[:12]],  # show only 12 chargers at once
     )
 
     return markup
+
+
+async def get_number_of_markup_buttons(markup: InlineKeyboardMarkup) -> int:
+    """Get the number of buttons in the markup"""
+    buttons = 0
+    for row in markup.inline_keyboard:
+        buttons += len(row)
+    return buttons
 
 
 async def cleanup(context: ContextTypes.DEFAULT_TYPE, update: Update = None):
@@ -131,7 +153,7 @@ async def cleanup(context: ContextTypes.DEFAULT_TYPE, update: Update = None):
     context.user_data.pop("no_chargers_found", None)
     if context.job:
         await context.bot.send_message(context.job.user_id, ended_sharing_live_loc)
-    else:
+    elif update:
         await update.effective_message.reply_text(ended_sharing_live_loc)
     print("Cleaned up")
 
